7b6e30d81a8ea3198ce666446b081428d5949b25
Sanjeev Soni <44490954+sanjeev0007@users.noreply.github.com>
2019-10-03 21:52:34 +0530

    Update User.js

--- amoc/server/models/User.js
+++ amoc/server/models/User.js
@@ -1,14 +1,17 @@
 var mongoose = require('mongoose'),
 
    UserSchema = new mongoose.Schema({
+      //user name
   name: {
     type: String,
     required: true
   },
+      //user email
   email: {
     type: String,
     required: true
   },
+      // create a password
   password: {
     type: String,
     required: true
